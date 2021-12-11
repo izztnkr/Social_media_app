c105aa0f9a8308e5491092792b03a1cedab94328
Ethan Chandler <86173119+IzzTinkr@users.noreply.github.com>
2021-12-11 13:40:48 -0600

    user controller and user routes created

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,17 +1,17 @@
-const router = require("express").Router();
-const {
-  addComment,
-  addReply,
-  removeComment,
-  removeReply,
-} = require("../../controllers/comment-controller");
+// const router = require("express").Router();
+// const {
+//   addComment,
+//   addReply,
+//   removeComment,
+//   removeReply,
+// } = require("../../controllers/comment-controller");
 
-// /api/comments/<pizzaId>
-router.route("/:pizzaId").post(addComment);
+// // /api/comments/<pizzaId>
+// router.route("/:pizzaId").post(addComment);
 
-// /api/comments/<pizzaId>/<commentId>
-router.route("/:pizzaId/:commentId").put(addReply).delete(removeComment);
+// // /api/comments/<pizzaId>/<commentId>
+// router.route("/:pizzaId/:commentId").put(addReply).delete(removeComment);
 
-router.route("/:pizzaId/:commentId/:replyId").delete(removeReply);
+// router.route("/:pizzaId/:commentId/:replyId").delete(removeReply);
 
-module.exports = router;
+// module.exports = router;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,16 +1,16 @@
 const router = require("express").Router();
 const {
-  getAllPizza,
-  getPizzaById,
-  createPizza,
-  updatePizza,
-  deletePizza,
-} = require("../../controllers/pizza-controller");
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require("../../controllers/user-controller");
 
-// Set up GET all and POST at /api/pizzas
-router.route("/").get(getAllPizza).post(createPizza);
+// Set up GET all and POST at /api/users
+router.route("/").get(getAllUsers).post(createUser);
 
-// Set up GET one, PUT, and DELETE at /api/pizzas/:id
-router.route("/:id").get(getPizzaById).put(updatePizza).delete(deletePizza);
+// Set up GET one, PUT, and DELETE at /api/users/:id
+router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
 
 module.exports = router;
